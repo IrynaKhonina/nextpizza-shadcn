0e0090117ce364019c99152069eaa2d9fa9f0fc1
IrynaKhonina <irinahonina@yahoo.com>
2025-12-01 18:31:30 +0300

    Categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import {Button} from "@/components/ui/button";
-
+import {Categories, Container, SortPopup, Title} from "@/components/shared";
 
 export default function Home() {
-  return (
-<h1>
-  Главная
-</h1>
-  );
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+            </Container>
+
+            <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5">
+                <Container className="flex items-center justify-between">
+                    <Categories />
+                    <SortPopup />
+                </Container>
+            </div>
+        </>
+    );
 }
