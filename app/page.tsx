2eb721d82e016a0fc0b5d8766df4ca896b948cbb
IrynaKhonina <irinahonina@yahoo.com>
2025-12-01 19:22:23 +0300

    RangeSlider

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import {Container, Filters, Title, TopBar} from "@/components/shared";
 export default function Home() {
     return (
         <>
-            <Container className="mt-10">
+            <Container className="mt-10 ">
                 <Title text="Все пиццы" size="lg" className="font-extrabold"/>
             </Container>
             <TopBar/>
 
 
 
-            <Container className="pb-14">
+            <Container className='mt-10 pb-14]'>
                 <div className="flex gap-[60px]">
 
                     {/*ФИЛЬТРАЦИЯ*/}
