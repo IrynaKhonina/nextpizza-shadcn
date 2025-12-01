39c19995a694257cc4475acfd991fc78bf6c5791
IrynaKhonina <irinahonina@yahoo.com>
2025-12-01 18:43:15 +0300

    TopBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import {Categories, Container, SortPopup, Title} from "@/components/shared";
+import {TopBar} from "@/components/shared/top-bar";
 
 export default function Home() {
     return (
         <>
             <Container className="mt-10">
                 <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+                <TopBar/>
             </Container>
 
-            <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5">
-                <Container className="flex items-center justify-between">
-                    <Categories />
-                    <SortPopup />
-                </Container>
-            </div>
+
         </>
     );
 }
